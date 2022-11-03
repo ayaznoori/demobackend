a522f1893d923f2f1fe0e32acf5c787e2be61551
ayaznoori <ayaz.noori15@gmail.com>
2022-11-03 09:59:39 +0530

    backend

--- Schema/userSchema.js
+++ Schema/userSchema.js
@@ -5,7 +5,7 @@ const userSchema=new mongoose.Schema({
     username:{type:String, required:true},
     email:{type: String, unique:true},
     password:{type:String, required:true},
-    tickets:[{category:String,title:String,description:String}]
+    tickets:[{category:String,title:String,description:String,date:Date}]
 })
 
 const userModel=mongoose.model('user',userSchema);
